064155f5c2890d8363d9ac6b6a5081098c26aa82
Eduardo Diaz <eduardiazf@gmail.com>
2021-06-11 11:11:01 -0400

    fixed

--- setup/do_setup.sh
+++ setup/do_setup.sh
@@ -3,11 +3,11 @@ git config --global user.email $EMAIL
 
 if [ ! -f /vagrant/setup/id_rsa ]; then
     ssh-keygen -b 2048 -t rsa -C $EMAIL -f /vagrant/setup/id_rsa -q -N ""
-
-    echo -e "Coping ssh keys"
-    cp /vagrant/setup/id_rsa /home/vagrant/.ssh/id_rsa
-    cp /vagrant/setup/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
-
-    echo -e "Change permissions to ssh keys"
-    chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
 fi
+
+echo -e "Coping ssh keys"
+cp /vagrant/setup/id_rsa /home/vagrant/.ssh/id_rsa
+cp /vagrant/setup/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
+
+echo -e "Change permissions to ssh keys"
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
